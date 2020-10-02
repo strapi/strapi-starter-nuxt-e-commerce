a0d7d97a72c758bc567d7bfdc6baf5ffc5e9690f
Roberth González <63687573+buzz-js@users.noreply.github.com>
2020-10-02 13:50:54 +0200

    refactor: Buttons overlapping
    Prevent buttons from overlapping on mobile devices

--- frontend/components/Buttons.vue
+++ frontend/components/Buttons.vue
@@ -4,7 +4,7 @@
     {{ error }}
   </div>
   <div v-else>
-    <div class="container mx-auto ml-4 mr-6 mt-8">
+    <div class="container flex flex-wrap mx-auto ml-4 mr-6 mt-8">
       <nuxt-link v-for="category in categories" :to="`/categories/${category.slug}`" :key="category.id" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded shadow">
         {{ category.name }}
       </nuxt-link>
