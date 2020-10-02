8de0dfd08615cbfeeb845f4ff52f8bb18f09114d
Roberth González <63687573+buzz-js@users.noreply.github.com>
2020-10-02 14:00:48 +0200

    refactor: Lazy load images

--- frontend/components/Products.vue
+++ frontend/components/Products.vue
@@ -7,7 +7,7 @@
     <div v-for="product in products" :key="product.id" class="border rounded-lg bg-gray-100 hover:shadow-lg shadow-md">
       <nuxt-link :to="`/products/${product.slug}`">
         <div class="rounded-t-lg bg-white pt-2 pb-2">
-          <img class="crop mx-auto" :src="`${getStrapiMedia(product.image.formats.thumbnail.url)}`">
+          <img v-lazy class="crop mx-auto" src="~/assets/img/placeholder-image.png" :data-src="`${getStrapiMedia(product.image.formats.thumbnail.url)}`">
         </div>
         <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
           <h4 class="mt-1 font-semibold text-base leading-tight truncate text-gray-700">{{product.title}} sticker</h4>
@@ -29,6 +29,22 @@ export default {
     products: Array,
     error: Object,
     storeUrl: String
+  },
+  directives: {
+    lazy: {
+      inserted: (el) => {
+        const observer = new IntersectionObserver((entries, observer) => {
+          entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+              let lazyImage = entry.target;
+              lazyImage.src = lazyImage.dataset.src;
+              observer.unobserve(el);
+            }
+          });
+        });
+        observer.observe(el);
+      },
+    },
   },
   methods: {
     getStrapiMedia
