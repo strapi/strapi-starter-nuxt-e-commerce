d4a1bd4ce9f8768230e2f773c8a29558a1e9c316
Roberth González <63687573+buzz-js@users.noreply.github.com>
2020-10-02 12:30:49 +0200

    refactor: The ssr Property
    "`mode` property change to `ssr` because mode option is deprecated.
    
    ssr: false // for SPA's

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -3,7 +3,7 @@ export default {
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
    */
-  mode: 'universal',
+  ssr: true,
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
